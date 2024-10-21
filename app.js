a7a6353732e8672563ae795ee117fba5b85c27de
Blake Peters <blakerdpeters@gmail.com>
2024-10-21 10:47:32 -0700

    Init

---
+++ app.js
@@ -0,0 +1,5 @@
+// turn based game with chests with loot
+// object face the dragon
+// if player has the 3 main objects, they can defeat the dragon 
+// event: raid against bandits (pros and cons for choices)
+// event: help random person (turns out they're a general!)
